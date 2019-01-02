b6ae366147cffe9986e6d82fd62574e86266d090
Rafsun Masud <rafsun82@gmail.com>
2019-01-02 21:48:46 +0600

    Responsive css file added

---
+++ .tools/send-android.bat
@@ -0,0 +1,2 @@
+@echo off
+start C:\Users\Rafsun\AppData\Local\Android\Sdk\tools\bin\avdmanager.bat -h
--- background.js
+++ background.js
@@ -1,27 +1,28 @@
 
 function focusOrCreateTab(url) {
-  browser.windows.getAll({"populate":true}, function(windows) {
-    var existing_tab = null;
-    for (var i in windows) {
-      var tabs = windows[i].tabs;
-      for (var j in tabs) {
-        var tab = tabs[j];
-        if (tab.url.replace("#","") == url) {
-          existing_tab = tab;
-          break;
-        }
-      }
-    }
-    if (existing_tab) {
+	var windows = browser.tabs.query({}); 
+	windows.then( function(tabs) {
+	var existing_tab = null;
+	for (let tab of tabs) {
+		  console.log(tab.url.replace("#",""));
+		if (tab.url.replace("#","") == url) {
+		  existing_tab = tab;
+		  break;
+		}
+	}
+    
+    if (existing_tab != null) {
 		browser.tabs.update(existing_tab.id, {"active":true});
     } else {
 		
 		browser.tabs.create({"url":url, "active":true});
-    }
-  });
+    } 
+	}, 
+	{});
 }
 
 browser.browserAction.onClicked.addListener(function(tab) {
   var manager_url = browser.extension.getURL("index.html");
+
   focusOrCreateTab(manager_url);
 });
